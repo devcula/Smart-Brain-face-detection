1d015265582ec38a6a3ac0c36744cac5c83d23a0
dEvCuLa <abhimaxfield@gmail.com>
2019-05-25 15:47:22 +0530

    Initial commit

---
+++ src/components/LogoComponent/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () =>{
+    return (
+        <div className="ma4 mt0">
+
+        </div>
+    )
+}
+
+export default Logo;
---
+++ src/components/NavComponent/Navigation.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import './Navigation.css';
+
+const Navigation = () =>{
+    return (
+        <nav>
+            <p className="f3 dim link pa3 white pointer">Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
