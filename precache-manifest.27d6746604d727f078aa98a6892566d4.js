d20f3ed48d371cfe8386420fb9c5b07ceedc4a39
dEvCuLa <abhimaxfield@gmail.com>
2019-06-14 20:21:27 +0530

    Updates

---
+++ precache-manifest.27d6746604d727f078aa98a6892566d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7821a72884d8819af5a0ec8f354d0d",
+    "url": "/Smart-Brain-face-detection/index.html"
+  },
+  {
+    "revision": "37690afc8096e80a6394",
+    "url": "/Smart-Brain-face-detection/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "681c6766134ace2add2c",
+    "url": "/Smart-Brain-face-detection/static/css/main.3ae72f73.chunk.css"
+  },
+  {
+    "revision": "37690afc8096e80a6394",
+    "url": "/Smart-Brain-face-detection/static/js/2.06cd73da.chunk.js"
+  },
+  {
+    "revision": "681c6766134ace2add2c",
+    "url": "/Smart-Brain-face-detection/static/js/main.4993f312.chunk.js"
+  },
+  {
+    "revision": "9f4bc50c48f83a256d16",
+    "url": "/Smart-Brain-face-detection/static/js/runtime~main.e5b18fdd.js"
+  },
+  {
+    "revision": "c41b5c2d59f3aaed9fb6da541f16a123",
+    "url": "/Smart-Brain-face-detection/static/media/logo.c41b5c2d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3967323a1780e9fc2bc4cd3b10908b91.js"
+  "/Smart-Brain-face-detection/precache-manifest.27d6746604d727f078aa98a6892566d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Smart-Brain-face-detection/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
