2619579358651cb6172e2a845c87f2c4dc6ebe05
dEvCuLa <abhimaxfield@gmail.com>
2019-06-17 08:40:09 +0530

    Tweaks and bug fixes

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -46,10 +46,10 @@ const Footer = () => {
                         This project is being developed by Abhishek Prasad. This website uses some of latest technologies including <strong>ReactJS, Redux, ExpressJS, NodeJS and Postgresql</strong>.
                     </p>
                     <div className="footer-icons">
-                        <a href="https://github.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                        <a href="https://in.linkedin.com/in/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-                        <a href="https://twitter.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
-                        <a href="https://www.instagram.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a href="https://github.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} title="Github" /></a>
+                        <a href="https://in.linkedin.com/in/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} title="Linkedin" /></a>
+                        <a href="https://twitter.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} title="Twitter" /></a>
+                        <a href="https://www.instagram.com/devcula" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} title="Instagram" /></a>
 
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './container/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+// import {createStore, applyMiddleware} from 'redux';
+import {createStore} from 'redux';
 import { rootReducer } from './redux/reducers';
 import { createLogger } from 'redux-logger';
 
 import 'tachyons';
 
-const logger = createLogger();
+// const logger = createLogger();
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}>
         <App />
