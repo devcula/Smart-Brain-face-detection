16753a795ca1c173b1de2e335aeae77aa36be709
dEvCuLa <abhimaxfield@gmail.com>
2019-06-17 08:42:07 +0530

    Tweaks and bug fixes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 // import {createStore, applyMiddleware} from 'redux';
 import {createStore} from 'redux';
 import { rootReducer } from './redux/reducers';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 
 import 'tachyons';
 
